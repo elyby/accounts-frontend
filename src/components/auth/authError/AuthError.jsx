5adfc607837eefb16f796fdd9c502d4734b89694
SleepWalker <mybox@udf.su>
2016-05-02 11:06:25 +0300

    Convert AuthError into stateless component

--- src/components/auth/authError/AuthError.jsx
+++ src/components/auth/authError/AuthError.jsx
@@ -1,25 +1,18 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 import errorsDict from 'services/errorsDict';
 import { PanelBodyHeader } from 'components/ui/Panel';
 
-export default class AuthError extends Component {
-    static displayName = 'AuthError';
-
-    static propTypes = {
-        error: PropTypes.string.isRequired,
-        onClose: PropTypes.func
-    };
-
-    render() {
-        let { error } = this.props;
-
-        error = errorsDict.resolve(error);
-
-        return (
-            <PanelBodyHeader type="error" onClose={this.props.onClose}>
-                {error}
-            </PanelBodyHeader>
-        );
-    }
+export default function AuthError({error, onClose = function() {}}) {
+    return (
+        <PanelBodyHeader type="error" onClose={onClose}>
+            {errorsDict.resolve(error)}
+        </PanelBodyHeader>
+    );
 }
+
+AuthError.displayName = 'AuthError';
+AuthError.propTypes = {
+    error: PropTypes.string.isRequired,
+    onClose: PropTypes.func
+};
