35d430e13ce4f214052254bbafdf35b24bd9026e
SleepWalker <mybox@udf.su>
2016-06-04 19:54:42 +0300

    #126: middleware feature for request service. Created middlewares for token headers and token refreshing

--- src/services/request.js
+++ src/services/request.js
@@ -26,43 +26,55 @@ function buildQuery(data = {}) {
         ;
 }
 
-function doFetch(...args) {
-    // NOTE: we are wrapping fetch, because it is returning
-    // Promise instance that can not be pollyfilled with Promise.prototype.finally
-    return new Promise((resolve, reject) => fetch(...args).then(resolve, reject));
-}
-
-let authToken;
+const middlewares = [];
 
 const checkStatus = (resp) => Promise[resp.status >= 200 && resp.status < 300 ? 'resolve' : 'reject'](resp);
 const toJSON = (resp) => resp.json();
 const rejectWithJSON = (resp) => toJSON(resp).then((resp) => {throw resp;});
-const handleResponse = (resp) => Promise[resp.success || typeof resp.success === 'undefined' ? 'resolve' : 'reject'](resp);
+const handleResponseSuccess = (resp) => Promise[resp.success || typeof resp.success === 'undefined' ? 'resolve' : 'reject'](resp);
 
-const getDefaultHeaders = () => {
-    const header = {Accept: 'application/json'};
+function doFetch(url, options = {}) {
+    // NOTE: we are wrapping fetch, because it is returning
+    // Promise instance that can not be pollyfilled with Promise.prototype.finally
 
-    if (authToken) {
-        header.Authorization = `Bearer ${authToken}`;
-    }
+    options.headers = options.headers || {};
+    options.headers.Accept = 'application/json';
 
-    return header;
-};
+    return runMiddlewares('before', {url, options})
+        .then(({url, options}) => fetch(url, options))
+        .then(checkStatus)
+        .then(toJSON, rejectWithJSON)
+        .then(handleResponseSuccess)
+        .then((resp) => runMiddlewares('then', resp))
+        .catch((resp) => runMiddlewares('catch', resp, () => doFetch(url, options)))
+        ;
+}
+
+/**
+ * @param  {string} action - the name of middleware's hook (before|then|catch)
+ * @param  {Object} data - the initial data to pass through middlewares chain
+ * @param  {Function} restart - a function to restart current request (for `catch` hook)
+ *
+ * @return {Promise}
+ */
+function runMiddlewares(action, data, restart) {
+    return middlewares
+        .filter((middleware) => middleware[action])
+        .reduce(
+            (promise, middleware) => promise.then((resp) => middleware[action](resp, restart)),
+            Promise[action === 'catch' ? 'reject' : 'resolve'](data)
+        );
+}
 
 export default {
     post(url, data) {
         return doFetch(url, {
             method: 'POST',
             headers: {
-                ...getDefaultHeaders(),
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
             },
             body: buildQuery(data)
-        })
-        .then(checkStatus)
-        .then(toJSON, rejectWithJSON)
-        .then(handleResponse)
-        ;
+        });
     },
 
     get(url, data) {
@@ -71,18 +83,14 @@ export default {
             url += separator + buildQuery(data);
         }
 
-        return doFetch(url, {
-            headers: getDefaultHeaders()
-        })
-        .then(checkStatus)
-        .then(toJSON, rejectWithJSON)
-        .then(handleResponse)
-        ;
+        return doFetch(url);
     },
 
     buildQuery,
 
-    setAuthToken(tkn) {
-        authToken = tkn;
+    addMiddleware(middleware) {
+        if (!middlewares.find((mdware) => mdware === middleware)) {
+            middlewares.push(middleware);
+        }
     }
 };
