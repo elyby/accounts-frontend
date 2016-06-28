95e7c0ae87465a56df764f2f79a766a15b236ff8
SleepWalker <mybox@udf.su>
2016-06-28 12:30:02 +0300

    #142: stop auto-scroll if user has tries to scroll manually

--- src/components/ui/scrollTo.js
+++ src/components/ui/scrollTo.js
@@ -7,24 +7,37 @@
 const requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame
     || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-const TIME_CONSTANT = 100;
+const TIME_CONSTANT = 100; // heigher numbers - slower animation
 export default function scrollTo(y, viewPort) {
     const start = Date.now();
     const {scrollTop} = viewPort;
     const amplitude = y - scrollTop;
+    let scrollWasTouched = false;
 
     requestAnimationFrame(function animateScroll() {
         const elapsed = Date.now() - start;
 
         let delta = -amplitude * Math.exp(-elapsed / TIME_CONSTANT);
 
-        if (Math.abs(delta) > 0.5) {
+        if (Math.abs(delta) > 0.5 && !scrollWasTouched) {
             requestAnimationFrame(animateScroll);
         } else {
             delta = 0;
+            document.removeEventListener('mousewheel', markScrollTouched);
+            document.removeEventListener('touchstart', markScrollTouched);
+        }
+
+        if (scrollWasTouched) {
+            return;
         }
 
         viewPort.scrollTop = y + delta;
     });
+
+    document.addEventListener('mousewheel', markScrollTouched);
+    document.addEventListener('touchstart', markScrollTouched);
+    function markScrollTouched() {
+        scrollWasTouched = true;
+    }
 }
 
