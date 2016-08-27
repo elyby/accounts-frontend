8cd00a5c405a64047e84e0a1598ed3e891bc621d
SleepWalker <mybox@udf.su>
2016-08-27 16:40:14 +0300

    Add a hack to overcome safary back-forward cache

--- src/polyfills.js
+++ src/polyfills.js
@@ -3,3 +3,9 @@ import 'promise.prototype.finally';
 
 // allow :active styles in mobile Safary
 document.addEventListener('touchstart', () => {}, true);
+
+// disable mobile safary back-forward cache
+// http://stackoverflow.com/questions/8788802/prevent-safari-loading-from-cache-when-back-button-is-clicked
+window.onpageshow = (event) => {
+    event.persisted && window.location.reload();
+};
