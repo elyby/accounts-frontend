ca7c0ed94ceba839862484c99750497c27dde250
SleepWalker <mybox@udf.su>
2017-01-14 21:38:38 +0200

    Async/await and new function bind syntax support

---
+++ scripts/test-async-await.js
@@ -0,0 +1,13 @@
+const test = async () => {
+};
+
+const obj = {a: 1, b: 1};
+
+const {a, b} = obj;
+
+console.log(a, b, 'ok');
+
+
+// how to:
+// cd to frontend
+// > ./node_modules/.bin/babel-node --presets es2015,es2017,stage-0 ./scripts/test-async-await.js
