a33d0b66d06133781ec242ed712a86fc845973d4
SleepWalker <mybox@udf.su>
2017-01-25 08:18:47 +0200

    Add Consolas webfont

--- src/components/ui/bsod/dispatchBsod.js
+++ src/components/ui/bsod/dispatchBsod.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { bsod } from './actions';
 import BSoD from 'components/ui/bsod/BSoD';
+import font from 'services/font';
 
 let injectedStore;
 let onBsod;
@@ -11,9 +12,11 @@ export default function dispatchBsod(store = injectedStore) {
     store.dispatch(bsod());
     onBsod && onBsod();
 
-    ReactDOM.render(
-        <BSoD store={store} />,
-        document.getElementById('app')
+    font.load(['Consolas']).then(() =>
+        ReactDOM.render(
+            <BSoD store={store} />,
+            document.getElementById('app')
+        )
     );
 }
 
--- src/services/font.js
+++ src/services/font.js
@@ -3,19 +3,29 @@ import webFont from 'webfontloader';
 export default {
     /**
      * @param {array} families
+     * @param {object} options
+     * @param {bool} [options.external=false] - whether the font should be loaded from external source (e.g. google)
      *
      * @return {Promise}
      */
-    load(families = []) {
+    load(families = [], options = {}) {
+        let config = {
+            custom: {families}
+        };
+
+        if (options.external) {
+            config = {
+                google: {families}
+            };
+        }
+
         return new Promise((resolve) =>
             webFont.load({
                 classes: false,
                 active: resolve,
                 inactive: resolve, // TODO: may be we should track such cases
                 timeout: 2000,
-                custom: {
-                    families
-                }
+                ...config
             })
         );
     }
