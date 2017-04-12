7a30a7e37f4744023d08b07c59cd47a75ddb0bdb
SleepWalker <mybox@udf.su>
2017-04-12 21:34:12 +0300

    #316: this time realy fixed Edge

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,19 +1,6 @@
 import 'babel-polyfill';
+import 'whatwg-fetch';
 import { shim as shimPromiseFinaly } from 'promise.prototype.finally';
-
-const isEdge = /Edge\//.test(navigator.userAgent);
-if (isEdge) {
-    // Edge has a broken fetch implementation, so forcing the polyfill
-    // https://www.reddit.com/r/webdev/comments/57ii4f/psa_edge_14_ships_a_broken_windowfetch/
-    // https://github.com/aurelia/fetch-client/issues/81
-    // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/9370062/
-    // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/7773267/
-    Reflect.deleteProperty(window, 'fetch');
-}
-
-// using require instead of import, because import is hoisting to the top
-// so that our fetch hack for Edge won't work
-require('whatwg-fetch');
 
 shimPromiseFinaly();
 
