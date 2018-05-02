dae803abfdb81a74473cbefb8a2fcc30a51611d9
SleepWalker <mybox@udf.su>
2018-05-02 22:08:31 +0300

    Fix bsod tests

--- src/components/ui/bsod/BsodMiddleware.test.js
+++ src/components/ui/bsod/BsodMiddleware.test.js
@@ -4,7 +4,7 @@ import sinon from 'sinon';
 import BsodMiddleware from 'components/ui/bsod/BsodMiddleware';
 
 describe('BsodMiddleware', () => {
-    [404, 500, 503, 555].forEach((code) =>
+    [500, 503, 555].forEach((code) =>
         it(`should dispatch for ${code}`, () => {
             const resp = {
                 originalResponse: {status: code}
@@ -26,20 +26,22 @@ describe('BsodMiddleware', () => {
         })
     );
 
-    it('should not dispatch for 200', () => {
-        const resp = {
-            originalResponse: {status: 200}
-        };
+    [200, 404].forEach((code) =>
+        it(`should not dispatch for ${code}`, () => {
+            const resp = {
+                originalResponse: {status: code}
+            };
 
-        const dispatch = sinon.spy();
-        const logger = {warn: sinon.spy()};
+            const dispatch = sinon.spy();
+            const logger = {warn: sinon.spy()};
 
-        const middleware = new BsodMiddleware(dispatch, logger);
+            const middleware = new BsodMiddleware(dispatch, logger);
 
-        return expect(middleware.catch(resp), 'to be rejected with', resp)
-            .then(() => {
-                expect(dispatch, 'was not called');
-                expect(logger.warn, 'was not called');
-            });
-    });
+            return expect(middleware.catch(resp), 'to be rejected with', resp)
+                .then(() => {
+                    expect(dispatch, 'was not called');
+                    expect(logger.warn, 'was not called');
+                });
+        })
+    );
 });
