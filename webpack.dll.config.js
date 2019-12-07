73e1af6e89188ec44551242da437e7f2e7da4772
SleepWalker <mybox@udf.su>
2019-12-07 22:01:48 +0200

    Fix dll webpack config

--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -2,8 +2,7 @@
 /* eslint-env node */
 const path = require('path');
 const webpack = require('webpack');
-const supportedLocales = require('app/i18n');
-const vendor = Object.keys(require('./package.json').dependencies);
+const vendor = Object.keys(require('./packages/app/package.json').dependencies);
 
 const isProduction = process.argv.some(arg => arg === '-p');
 const isTest = process.argv.some(arg => arg.indexOf('karma') !== -1);
@@ -24,20 +23,7 @@ const webpackConfig = {
   mode: 'development',
 
   entry: {
-    vendor: vendor
-      .concat([
-        'core-js/library',
-        'redux-devtools',
-        'redux-devtools-dock-monitor',
-        'redux-devtools-log-monitor',
-        'react-transform-hmr',
-        'react-transform-catch-errors',
-      ])
-      .concat(
-        Object.keys(supportedLocales).map(
-          locale => `react-intl/locale-data/${locale}.js`,
-        ),
-      ),
+    vendor,
   },
 
   output: {
@@ -47,15 +33,12 @@ const webpackConfig = {
     publicPath: '/',
   },
 
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: ['./packages/app/node_modules', './node_modules'],
+  },
+
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-      __DEV__: true,
-      __TEST__: isTest,
-      __PROD__: false,
-    }),
     new webpack.DllPlugin({
       name: '[name]',
       path: path.join(outputPath, '[name].json'),
