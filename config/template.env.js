eee8f257e3f171a6e07209a210f617d56d1d005e
ErickSkrauch <erickskrauch@yandex.ru>
2020-01-09 16:27:49 +0300

    Remove old config file

--- config/template.env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  version: 'dev',
-  apiHost: 'https://dev.account.ely.by',
-  ga: { id: 'UA-XXXXX-Y' },
-  sentryCdn: 'https://<key>@sentry.io/<project>',
-  crowdinApiKey: '',
-};
