dfa8c6df5f8731debb16322d7529aca173b5cfed
Sviatoslav <mybox@udf.su>
2020-01-16 17:33:05 +0200

    Fix sb build (closes #18) (#19)
    * Fix storybook build command
    
    * Bump deps
    
    * Add storybook build step
    
    * Perform storybook build quiet
    
    Co-authored-by: ErickSkrauch <erickskrauch@ely.by>

--- babel.config.js
+++ babel.config.js
@@ -14,8 +14,6 @@ module.exports = {
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-proposal-function-bind',
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
     [
       '@babel/plugin-transform-runtime',
       {
