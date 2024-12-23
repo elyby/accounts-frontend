f96e5318120b6f3532113a14bf3d1fa611b278dd
ErickSkrauch <erickskrauch@yandex.ru>
2024-12-23 15:13:10 +0100

    Update the design

--- packages/app/components/auth/deviceCode/DeviceCodeBody.tsx
+++ packages/app/components/auth/deviceCode/DeviceCodeBody.tsx
@@ -4,6 +4,8 @@ import { FormattedMessage as Message } from 'react-intl';
 import { Input } from 'app/components/ui/form';
 import BaseAuthBody from 'app/components/auth/BaseAuthBody';
 
+import style from './deviceCode.scss';
+
 export default class DeviceCodeBody extends BaseAuthBody {
     static displayName = 'DeviceCodeBody';
     static panelId = 'deviceCode';
@@ -15,12 +17,21 @@ export default class DeviceCodeBody extends BaseAuthBody {
             <>
                 {this.renderErrors()}
 
+                <div className={style.icon} />
+
+                <div className={style.description}>
+                    <Message
+                        id="deviceCodeDescription"
+                        defaultMessage="To authorize the application, enter the code displayed on the screen."
+                    />
+                </div>
+
                 <Message id="enterDeviceCode" defaultMessage="Enter Device Code">
                     {(nodes) => (
                         <Input
                             {...this.bindField('user_code')}
-                            icon="key"
                             autoFocus
+                            center
                             required
                             placeholder={nodes as string}
                         />
